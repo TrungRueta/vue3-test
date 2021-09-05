6be2dcafc3ba45a51fe47af9dbd77532f98d2642
TrungRueta <trungrueta@icloud.com>
2021-09-05 08:55:01 +0700

    add test incorrect type checking sugar ref

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,18 @@
 enum MyEnum {
   test = "true",
 }
+
+// this hint correct null | boolean type
+let a = $ref(null as null | boolean);
+
+// this lost null check
+interface Ok {
+  ok: boolean;
+}
+let b = $ref(null as null | Ok);
+
+// this should be mark error by typescript. Should be b?.ok
+b.ok;
 </script>
 
 <template>
